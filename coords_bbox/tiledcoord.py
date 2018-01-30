03d540a2f5087f65517c27844ac4a15acedd2a1d
abe- <info@croopier.com>
2018-01-30 22:57:06 +0100

    corrected tiles

--- coords_bbox/tiledcoord.py
+++ coords_bbox/tiledcoord.py
@@ -5,26 +5,12 @@ import random
 from datetime import datetime
 import numpy as np
 
-
-# http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/
-
-def PixelsToLatLon(x, y, zoom):
-	res = 180 / 256.0 / 2**zoom
-	lat = x*res-180
-	lon = y*res-90
-	return lat, lon
-
-def LatLonToPixels(lat, lon, zoom):
-        "Converts lat/lon to pixel coordinates in given zoom of the EPSG:4326 pyramid"
-        res = 180 / 256.0 / 2**zoom
-        x = (180 + lat) / res
-        y = (90 + lon) / res
-        return x, y
-
-
-random.seed(datetime.now())
-
 # Online tool to retrieve bbox lat/lon boundaries:
+# 
+#           DON'T FORGET TO CHOOSE THE 
+#      CORRECT ORDER OF COORDINATES Lat/Lng 
+#   (corner at the bottom right of the window)
+#
 # http://bboxfinder.com
 
 coords = []
@@ -63,20 +49,20 @@ coords = []
 #zoom=8
 
 #yu + groed
-key="ice"
-zoom=8
-coords.append( [71.635993,53.162842,73.131322,55.458984]  )
-coords.append( [73.201317,54.843750,74.116047,56.997070]  )
-coords.append( [74.132576,55.728149,74.849236,59.891968]  )
-coords.append( [74.873624,56.343384,75.336721,60.842285] )
-coords.append( [69.710489,-29.003906,74.164085,-21.093750] )
-coords.append( [74.188052,-23.071289,77.851100,-18.105469] )
-coords.append( [63.074866,-53.876953,68.942607,-47.548828] )
-coords.append( [68.997802,-55.942383,72.154890,-49.614258] )
-coords.append( [68.366801,-70.971680,70.801366,-67.170410] )
-coords.append( [70.510241,-73.366699,71.667119,-70.400391] )
-coords.append( [71.052665,-76.245117,72.143102,-73.108521] )
-coords.append( [71.732662,-78.332520,72.711903,-74.904785] )
+#key="ice"
+#zoom=8
+#coords.append( [71.635993,53.162842,73.131322,55.458984]  )
+#coords.append( [73.201317,54.843750,74.116047,56.997070]  )
+#coords.append( [74.132576,55.728149,74.849236,59.891968]  )
+#coords.append( [74.873624,56.343384,75.336721,60.842285] )
+#coords.append( [69.710489,-29.003906,74.164085,-21.093750] )
+#coords.append( [74.188052,-23.071289,77.851100,-18.105469] )
+#coords.append( [63.074866,-53.876953,68.942607,-47.548828] )
+#coords.append( [68.997802,-55.942383,72.154890,-49.614258] )
+#coords.append( [68.366801,-70.971680,70.801366,-67.170410] )
+#coords.append( [70.510241,-73.366699,71.667119,-70.400391] )
+#coords.append( [71.052665,-76.245117,72.143102,-73.108521] )
+#coords.append( [71.732662,-78.332520,72.711903,-74.904785] )
 
 # jiparana
 #lat0,lon0,lat1,lon1=[-12.343831,-62.641296,-10.734638,-61.042786]
@@ -89,8 +75,43 @@ coords.append( [71.732662,-78.332520,72.711903,-74.904785] )
 #key="crops"
 #zoom = 13
 
+# edson, kansas (irrigation agriculture)
+coords.append( [ 40.165757,-101.698380,40.239178,-101.553497 ] )
+zoom = 13
+key = "edson"
+
+
+
+
+
+# helper functions
+# http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/
 # coords transformed from EPSG:4326 (lat/lon) to EPSG:3857 (web mercator)
+
+
+def PixelsToLatLon(x, y, zoom):
+	res = 180 / 256.0 / 2**zoom
+	lat = x*res-180
+	lon = y*res-90
+	return lat, lon
+
+def LatLonToPixels(lat, lon, zoom):
+        "Converts lat/lon to pixel coordinates in given zoom of the EPSG:4326 pyramid"
+        res = 180 / 256.0 / 2**zoom
+        x = (180 + lat) / res
+        y = (90 + lon) / res
+        return x, y
+
+
+
+
+
+
+# json producer
+
+
 data = []
+random.seed(datetime.now())
 
 count = 0
 for cds in coords:
