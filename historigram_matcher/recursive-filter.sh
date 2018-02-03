27c6d57b10f25109f3a8af57ef9a4f353adc4df6
abe- <info@croopier.com>
2018-02-03 08:27:04 +0100

    recursive filter

--- historigram_matcher/recursive-filter.sh
+++ historigram_matcher/recursive-filter.sh
@@ -8,7 +8,7 @@ echo "Changing the colour levels of" $first
 convert $first -level 25% $first;
 echo "Adjusting the rest of histograms to " $first
 for j in $( ls|tail -n32 );
-do bash ../histmatch.sh $first $j $j;
+do bash ../../../histmatch.sh $first $j $j;
 done;
 cd ..;
 done;
