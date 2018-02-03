c89610d264c4ce9e0f8a1acd8fd058693767cc0d
abe- <info@croopier.com>
2018-02-03 07:58:56 +0100

    updated recursive filter

--- historigram_matcher/recursive-filter.sh
+++ historigram_matcher/recursive-filter.sh
@@ -1,13 +1,16 @@
-for i in $( ls -d */ ); do 
-echo "Entering dir " $i; 
-cd $i; 
+cd Data;
+cd Train;
+for i in $( ls -d */ ); do
+echo "Entering dir " $i;
+cd $i;
 first=`ls|head -n1`
 echo "Changing the colour levels of" $first
-convert $first -level 25% $first; 
+convert $first -level 25% $first;
 echo "Adjusting the rest of histograms to " $first
-for j in $( ls|tail -n32 ); 
-do bash ../histmatch.sh $first $j $j; 
-done; 
-cd ..; 
+for j in $( ls|tail -n32 );
+do bash ../histmatch.sh $first $j $j;
 done;
-
+cd ..;
+done;
+cd ..;
+cd ..;
